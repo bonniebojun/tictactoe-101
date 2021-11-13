ebd5f14301f5585d94fd64dec6df75656e4e3361
Bonnie Jun <bonniebojun@gmail.com>
2021-11-13 00:23:39 -0600

    TTT-Logic

--- scripts.js
+++ scripts.js
@@ -35,8 +35,16 @@ const handleClick = (element) => {
   }
 }
 
+// if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+// || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O") )
 
+// if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
+// || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+// )
 
+// if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+// || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
+// )
 
 
 
@@ -61,13 +69,6 @@ const addMarker = (id) => {
   // .innerHTML 
   changeMarker()
 }
-
-
-
-
-
-
-
 
 
 
